8365542ecc14c2daf0422e1c8cc4bc32e91c4962
Sergey Babich <babichsa.ua@gmail.com>
2021-08-16 12:14:51 +0300

    vue

---
+++ app/frontend/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    {{ test }}
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      test: "HelloWorld",
+    };
+  },
+};
+</script>
+
+<style></style>
